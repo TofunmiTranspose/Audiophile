de9bc65c0f2f68eb1c507019da5a630d2a1ea1f0
TransposeDev <tofexzyno@gmail.com>
2025-10-31 10:24:23 +0100

    new

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { FaHome } from "react-icons/fa";
+
+function App() {
+  return (
+    <div className="font-bold text-4xl flex gap-2">
+      Hello TSX{" "}
+      <span className="text-green-500">
+        <FaHome />
+      </span>{" "}
+    </div>
+  );
+}
+
+export default App;
